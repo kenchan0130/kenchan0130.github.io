e9f775b73074cb1f6db05aa92a0abbe4c7b1f468
Tadayuki Onishi <tt.tanishi100@gmail.com>
2018-05-19 23:56:55 +0900

    script を直書きではなく外部呼び出しするようにした

---
+++ js/external-links-new-window.js
@@ -0,0 +1,6 @@
+$(function() {
+  // Open extranal link to new window
+  $(document.links).filter(function() {
+    return this.hostname != window.location.hostname;
+  }).attr('target', '_blank');
+});
