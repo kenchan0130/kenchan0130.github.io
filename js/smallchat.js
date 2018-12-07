db2549bfaebe7a7ff9ddc396635316c005cf6036
kenchan0130 <tt.tanishi100@gmail.com>
2018-12-07 19:15:25 +0900

    Add open chat link.

--- js/smallchat.js
+++ js/smallchat.js
@@ -1,18 +1,7 @@
 $(function(){
-  // Add smallchat
-  $(window).on('scroll', function() {
-    var smallchatScriptDomId = 'kenchan0130-smallchat';
+  var smallchatScriptDomId = 'kenchan0130-smallchat';
 
-    if (document.getElementById(smallchatScriptDomId) !== null) {
-      return;
-    }
-
-    var docHeight = $(document).innerHeight();
-    var windowHeight = $(window).innerHeight();
-    var pageBottom = docHeight - windowHeight;
-    var buffer = $('footer').innerHeight() * 2.75;
-
-    if ((pageBottom - buffer) <= $(window).scrollTop()) {
+  var showSmallchat = function(smallchatScriptDomId, smallchatOnloadCallback) {
       var smallchatScript = document.createElement('script');
       smallchatScript.id = smallchatScriptDomId;
       smallchatScript.async = true;
@@ -25,10 +14,55 @@ $(function(){
         document.head.appendChild(styles);
         var script = document.createElement('script');
         script.src = '//static.small.chat/messenger.js';
+        if (typeof smallchatOnloadCallback === 'function') {
+          script.onload = smallchatOnloadCallback();
+        }
         script.async = true;
         document.body.appendChild(script);
       };
       smallchatScript.src = '//embed.small.chat/TAEV2JR5WGAS7P6N2V.js';
+  };
+
+  $('.js-open-smallchat').on('click', function() {
+    var openSmallchat = function() {
+      function openSmallchatLoop(counter) {
+        var $smallchatIframContent = $('#Smallchat iframe').contents();
+        // Kill the openSmallchat recursive call.
+        if (counter > 10) {
+          return;
+        }
+        // Retry opening smallchat
+        if ($smallchatIframContent.length > 0) {
+          $smallchatIframContent.find('.Icon').not('.Messenger_close').click();
+        } else {
+          openSmallchatLoop(counter + 1);
+        }
+      };
+      setTimeout(function() {
+        openSmallchatLoop(0);
+      }, 100);
+    };
+
+    if (document.getElementById(smallchatScriptDomId) !== null) {
+      openSmallchat();
+    } else {
+      showSmallchat(smallchatScriptDomId, openSmallchat);
+    }
+  });
+
+  // Show chat just before finishing reading
+  $(window).on('scroll', function() {
+    if (document.getElementById(smallchatScriptDomId) !== null) {
+      return;
+    }
+
+    var docHeight = $(document).innerHeight();
+    var windowHeight = $(window).innerHeight();
+    var pageBottom = docHeight - windowHeight;
+    var buffer = $('footer').innerHeight() * 2.75;
+
+    if ((pageBottom - buffer) <= $(window).scrollTop()) {
+      showSmallchat(smallchatScriptDomId);
     }
   });
 });
