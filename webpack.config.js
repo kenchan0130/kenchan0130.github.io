2b406560e5c9f7a6003468aa389fc6b4426ecd88
kenchan0130 <tt.tanishi100@gmail.com>
2019-05-17 13:39:53 +0900

    fix format js

--- webpack.config.js
+++ webpack.config.js
@@ -6,83 +6,83 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.m?js$/,
-				loader: 'babel-loader',
-				options: {
-					sourceType: 'unambiguous',
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								targets: {
-          				ie: "11"
-        				},
-								useBuiltIns: 'entry',
-								modules: false,
-								corejs: 3
-							}
-						],
-					]
-				},
-			},
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: [
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        loader: 'babel-loader',
+        options: {
+          sourceType: 'unambiguous',
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  ie: "11"
+                },
+                useBuiltIns: 'entry',
+                modules: false,
+                corejs: 3
+              }
+            ],
+          ]
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
         ],
-			},
-			{
-				test: /\.(ttf|eot|woff|woff2|svg|png|jpg|gif|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: 'file-loader'
-			}
-		]
-	},
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2|svg|png|jpg|gif|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
 
-	entry: {
-		app: './webpack/src/index.ts'
-	},
+  entry: {
+    app: './webpack/src/index.ts'
+  },
 
-	output: {
-		filename: '[name].[chunkhash].js',
-		path: path.resolve(__dirname, './assets/bundle')
-	},
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, './assets/bundle')
+  },
 
-	resolve: {
+  resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json']
   },
 
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/,
-				}
-			},
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/,
+        }
+      },
 
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	},
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  },
 
-	plugins: [
-		new AssetsManifest({
+  plugins: [
+    new AssetsManifest({
       output: path.resolve(__dirname, '_data', 'manifest.json')
     }),
-		new CleanWebpackPlugin({
-			cleanOnceBeforeBuildPatterns: ['**/*', '!.gitkeep']
-		}),
-		new MiniCssExtractPlugin({filename: '[name].[chunkhash].css'}),
-	]
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*', '!.gitkeep']
+    }),
+    new MiniCssExtractPlugin({filename: '[name].[chunkhash].css'}),
+  ]
 };
--- webpack/src/types/tablesorter.d.ts
+++ webpack/src/types/tablesorter.d.ts
@@ -1,3 +1,3 @@
 interface JQuery {
-	tablesorter(): JQuery;
+  tablesorter(): JQuery;
 }
