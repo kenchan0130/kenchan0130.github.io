bd1ddd98658a3e983df0dc3029be49cb898b9609
TADAYUKI ONISHI <tt.tanishi100@gmail.com>
2019-05-18 01:41:22 +0900

    fix lint command bugs

--- webpack/src/enable-sentry.ts
+++ webpack/src/enable-sentry.ts
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/browser'
 
 Sentry.init({
-  dsn: 'https://965e22e3acb4498186d88893f18c8e99@sentry.io/1454814',
+  dsn: 'https://965e22e3acb4498186d88893f18c8e99@sentry.io/1454814'
 })
