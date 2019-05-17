903a06c72c525f5f1245f193e3c79dc29d3f05ec
Tadayuki Onishi <tt.tanishi100@gmail.com>
2019-05-18 01:43:58 +0900

    Optimize development env (tslint & editorconfig) (#31)
    * adjust editor config
    
    * innovate tslint and prettier
    
    * fix ci buiid settings because it fixed lint
    
    * apply tslint for ts files
    
    * fix lint command bugs

--- webpack/src/smallchat.ts
+++ webpack/src/smallchat.ts
@@ -1,123 +1,125 @@
-import * as $ from 'jquery';
+import * as $ from 'jquery'
 
-export default $(() => {
-  const showedSmallchatDataKey = "showed-smallchat";
+const showedSmallchatDataKey = 'showed-smallchat'
 
-  const setSmallchatScript = (callback: () => void) => {
-    const script = document.createElement('script');
-    script.src = '//embed.small.chat/TAEV2JR5WGAS7P6N2V.js';
-    script.async = true;
-    script.onload = callback;
-    document.body.appendChild(script);
-  };
+const setSmallchatScript = (callback: () => void) => {
+  const script = document.createElement('script')
+  script.src = '//embed.small.chat/TAEV2JR5WGAS7P6N2V.js'
+  script.async = true
+  script.onload = callback
+  document.body.appendChild(script)
+}
 
-  const hideSmallchat = () => {
-    function hideSmallchatLoop(counter: number) {
-      const $smallchatDom = $('#Smallchat');
-      const upper = 10000000;
-      if ($smallchatDom.data(showedSmallchatDataKey) || counter > upper) {
-        return;
-      }
-
-      if ($smallchatDom.length > 0) {
-        $smallchatDom.hide();
-        return;
-      }
-
-      setTimeout(() => {
-        hideSmallchatLoop(counter + 1);
-      });
+const hideSmallchat = () => {
+  function hideSmallchatLoop(counter: number) {
+    const $smallchatDom = $('#Smallchat')
+    const upper = 10000000
+    if ($smallchatDom.data(showedSmallchatDataKey) || counter > upper) {
+      return
     }
+
+    if ($smallchatDom.length > 0) {
+      $smallchatDom.hide()
+      return
+    }
+
     setTimeout(() => {
-      hideSmallchatLoop(0);
-    });
-  };
+      hideSmallchatLoop(counter + 1)
+    })
+  }
+  setTimeout(() => {
+    hideSmallchatLoop(0)
+  })
+}
 
-  const showSmallchat = (showedCallback?: () => void) => {
-    function showSmallchatLoop(counter: number) {
-      const $smallchatDom = $('#Smallchat');
-      const upper = 1000;
-      if ($smallchatDom.data(showedSmallchatDataKey) || counter > upper) {
-        if (typeof showedCallback === 'function') {
-          showedCallback();
-        }
-        return;
+const showSmallchat = (showedCallback?: () => void) => {
+  function showSmallchatLoop(counter: number) {
+    const $smallchatDom = $('#Smallchat')
+    const upper = 1000
+    if ($smallchatDom.data(showedSmallchatDataKey) || counter > upper) {
+      if (typeof showedCallback === 'function') {
+        showedCallback()
       }
-
-      if ($smallchatDom.length > 0) {
-        if (typeof showedCallback === 'function') {
-          $smallchatDom.show(400, showedCallback);
-        } else {
-          $smallchatDom.show(400);
-        }
-        $smallchatDom.data(showedSmallchatDataKey, true);
-        return;
-      }
-
-      setTimeout(() => {
-        showSmallchatLoop(counter + 1);
-      });
+      return
     }
+
+    if ($smallchatDom.length > 0) {
+      if (typeof showedCallback === 'function') {
+        $smallchatDom.show(400, showedCallback)
+      } else {
+        $smallchatDom.show(400)
+      }
+      $smallchatDom.data(showedSmallchatDataKey, true)
+      return
+    }
+
     setTimeout(() => {
-      showSmallchatLoop(0);
-    });
-  };
+      showSmallchatLoop(counter + 1)
+    })
+  }
+  setTimeout(() => {
+    showSmallchatLoop(0)
+  })
+}
 
-  const decideTofinishReadingPage = (finishedReadingPageCallback: (_?: any) => void) => {
-    const docHeight = $(document).innerHeight();
-    const windowHeight = $(window).innerHeight();
-    const footerHeight = $("footer").innerHeight();
-    const scrollTop = $(window).scrollTop();
-    if (!docHeight || !windowHeight || !footerHeight || !scrollTop) {
-      return;
-    }
+const decideTofinishReadingPage = (
+  finishedReadingPageCallback: (_?: any) => void
+) => {
+  const docHeight = $(document).innerHeight()
+  const windowHeight = $(window).innerHeight()
+  const footerHeight = $('footer').innerHeight()
+  const scrollTop = $(window).scrollTop()
+  if (!docHeight || !windowHeight || !footerHeight || !scrollTop) {
+    return
+  }
 
-    const pageBottom = docHeight - windowHeight;
-    const buffer = footerHeight * 2.75;
+  const pageBottom = docHeight - windowHeight
+  const buffer = footerHeight * 2.75
 
-    if (pageBottom - buffer <= scrollTop) {
-      finishedReadingPageCallback();
-    }
-  };
+  if (pageBottom - buffer <= scrollTop) {
+    finishedReadingPageCallback()
+  }
+}
 
+$(() => {
   setSmallchatScript(() => {
     // When the user click DOM for opening chat, this script opens the chat.
-    $(".js-open-smallchat").on("click", () => {
+    $('.js-open-smallchat').on('click', () => {
       const openSmallchat = () => {
         function openSmallchatLoop(counter: number) {
-          const $smallchatIframContent = $("#Smallchat iframe").contents();
+          const $smallchatIframContent = $('#Smallchat iframe').contents()
           // Kill the openSmallchatLoop function recursive call.
-          const upper = 1000;
+          const upper = 1000
           if (counter > upper) {
-            return;
+            return
           }
           // Retry opening the chat
           if ($smallchatIframContent.length > 0) {
             $smallchatIframContent
-            .find(".Icon")
-            .not(".Messenger_close")
-            .click();
+              .find('.Icon')
+              .not('.Messenger_close')
+              .click()
           } else {
             setTimeout(() => {
-              openSmallchatLoop(counter + 1);
-            });
+              openSmallchatLoop(counter + 1)
+            })
           }
         }
         setTimeout(() => {
-          openSmallchatLoop(0);
-        });
-      };
-      showSmallchat(openSmallchat);
-    });
+          openSmallchatLoop(0)
+        })
+      }
+      showSmallchat(openSmallchat)
+    })
 
     // Hide chat icon by befault
-    hideSmallchat();
+    hideSmallchat()
     // If the user reload the page and fullfill showing smallchat, it show the chat.
-    decideTofinishReadingPage(showSmallchat);
+    decideTofinishReadingPage(showSmallchat)
 
     // Show the chat just before finishing reading with scroll event.
-    $(window).on("scroll", () => {
-      decideTofinishReadingPage(showSmallchat);
-    });
-  });
-});
+    $(window).on('scroll', () => {
+      decideTofinishReadingPage(showSmallchat)
+    })
+  })
+})
