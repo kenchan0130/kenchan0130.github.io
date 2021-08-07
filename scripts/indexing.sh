8a701c05c470d98b8584397bbcce89679fb8b62a
kenchan0130 <tt.tanishi100@gmail.com>
2021-08-08 00:20:13 +0900

    fix ci scripts

--- scripts/indexing.sh
+++ scripts/indexing.sh
@@ -5,12 +5,7 @@ if [[ ! "${ALGOLIA_ADMIN_API_KEY}" ]];then
   exit 1
 fi
 
-if [[ ! "${GITHUB_TOKEN}" ]];then
-  echo "'GITHUB_TOKEN' is requried"
-  exit 1
-fi
-
 echo "Create index items..."
 
-SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
-ALGOLIA_API_KEY="${ALGOLIA_ADMIN_API_KEY}" bundle exec jekyll algolia --config "${SCRIPT_DIR}/../_config.yml,${SCRIPT_DIR}/../_config_production.yml"
+script_dir=$( cd "$( dirname "$0" )" && pwd )
+ALGOLIA_API_KEY="${ALGOLIA_ADMIN_API_KEY}" bundle exec jekyll algolia --config "${script_dir}/../_config.yml,${script_dir}/../_config_production.yml"
